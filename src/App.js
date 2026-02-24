d4d62964787cd02cf8bbc3de79dc65401100180f
Sun <2194474271@qq.com>
2026-02-24 13:11:31 +0800

    调整

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
 // 引入Header、Hero组件
-import Header from './components/Header';
-import Hero from './components/Hero';
+import Header from './components/Header.js';
+import Hero from './components/Hero.js';
 
 function App() {
     return (
